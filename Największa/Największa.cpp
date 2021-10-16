3329f53aaa3a9b0a686e3a8221f79bb6d6c2f38d
rudyw <rudyw@DESKTOP-FTR59LE>
2021-10-16 17:35:38 +0200

    Dodaj pliki projektów.

---
+++ Największa/Największa.cpp
@@ -0,0 +1,33 @@
+﻿# include < iostream >
+# include < string >
+auto ask_user_for_integer(std::string const prompt) -> int
+{
+	if (not prompt.empty()) {
+		std::cout << prompt;
+	}
+	auto value = std::string{};
+	std::getline(std::cin, value);
+	return std::stoi(value);
+}
+
+int main()
+{
+	using namespace std;
+
+	while (true)
+	{
+		int a, b, c;
+		a = ask_user_for_integer("");
+		b = ask_user_for_integer("");
+		c = ask_user_for_integer("");
+		if ( (a > c && c > b) || (a > b && b > c) )
+			cout << a << '\n';
+		if ( (c > b && c > a) || (b > a && a > b) )
+			cout << c << '\n';
+		if ( (b > c && b > a) || (c > a && a > c) )
+			cout << b << '\n';
+
+			
+
+	}
+}
