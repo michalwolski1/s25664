38ec6afcd7cffedd1d345f30d924ab534e01ba63
michalwolski1 <91740734+michalwolski1@users.noreply.github.com>
2021-11-06 16:39:13 +0100

    Add files via upload

---
+++ liczba pierwsza.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+auto ask_user_for_integer(std::string const prompt) -> int
+{
+    if (not prompt.empty()) {
+        std::cout << prompt;
+    }
+    auto value = std::string{};
+    std::getline(std::cin, value);
+    return std::stoi(value);
+}
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    a = ask_user_for_integer("");
+    b = 0;
+
+        if (a > 2)
+
+            for (int i = 2; i * i <= a; i++)
+                if (a % i == 0)
+                    b++;
+        if (b == 0)
+            cout << "liczba jest pierwsza" << endl;
+        else
+            cout << "liczba nie jest pierwsza" << endl;
+}
+        
+     
---
+++ ralacja między liczbami 2.cpp
@@ -0,0 +1,32 @@
+﻿# include < iostream >
+# include < string >
+auto ask_user_for_integer(std::string const prompt) -> int
+{
+	if (not prompt.empty()) {
+		std::cout << prompt;
+	}
+	auto value = std::string{};
+	std::getline(std::cin, value);
+	return std::stoi(value);
+}
+void relacja(int a, int b)
+{
+	if (a == b)
+		std::cout << a << " == " << b << '\n';
+	else if (a < b)
+		std::cout << a << " < " << b << '\n';
+	else
+		std::cout << a << " > " << b << '\n';
+}
+int main()
+
+{
+	int tab[100], x, y;
+		x = ask_user_for_integer(""); //ilość liczb w tabeli
+		y = ask_user_for_integer(""); //liczba z którą tab[i] są porównywane
+		for (int i = 0; i < x; i++)
+			tab[i] = ask_user_for_integer(""); //liczby wpisane z klawiatury (do porównania z x)
+		for (int i = 0; i < x; i++)
+			relacja(y, tab[i]);
+	return 0;
+}
